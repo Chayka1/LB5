af1409eebd5fb4013a7bf254bfcd1eb846d16aa8
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2022-10-22 11:48:08 +0300

    LB5

---
+++ LB5_1/LB5_1/Printer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Printer
+{
+    protected ConsoleColor color = Console.ForegroundColor;
+
+    public void Print(string value)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(value);
+    }
+}
+
+class Company : Printer
+{
+    public Company()
+    {
+        color = ConsoleColor.Green;
+    }
+}
+
+class Position : Printer
+{
+    public Position()
+    {
+        color = ConsoleColor.Blue;
+    }
+}
+
+class NamePerson : Printer
+{
+    public NamePerson()
+    {
+        color = ConsoleColor.Red;
+    }
+}
